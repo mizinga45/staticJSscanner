818cc7b351aae1133ee97470ed4e20453508fe76
mizinga45 <mizinga45@users.noreply.github.com>
2026-05-27 03:04:26 +0300

    Fix legacy SQLAlchemy warning in user_loader

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 
 # Register blueprints
